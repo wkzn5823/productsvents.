2240f8263ddf0d1e2b6665ede438c36b1b0e4db8
wkzn5823 <alexisencarnacion5823@gmail.com>
2025-03-08 19:11:41 -0600

    Cambiando Variables2

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -2,6 +2,7 @@ const {config} = require('dotenv')
 config()
 
 module.exports = {
+    PORT: process.env.PORT,
     SERVER_URL: process.env.SERVER_URL,
     CLIENT_URL: process.env.CLIENT_URL,
     SECRET: process.env.SECRET,
